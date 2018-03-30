f0b05203cc855cee0bc66010a78bf215581313e3
wuke1993 <2297184250@qq.com>
2018-03-30 18:05:44 +0800

    使用数组存储节点、边等数据，增强可读性

--- src/relation/GenLinks.java
+++ src/relation/GenLinks.java
@@ -10,61 +10,70 @@ import utility.StoreStringIntoFile;
 * @author : wuke
 * @date   : 20171221 09:16:53
 * Title   : GenLinks
-* Description : 按照知识点和视频统一编号，共2211个，对 links 进行处理，生成可直接用于邻接矩阵生成的边
+* Description : 按照知识点和视频统一编号，从 0 开始，共1197个；生成links，共2211条，可直接用于邻接矩阵生成的边
 */
 public class GenLinks {
 	
-	private static String LINKS_PATH = "e:\\data\\my_kus\\links_4th_semester_kus_videos 2211.txt";
-	private static String LINKS_PATH_FINAL = "e:\\data\\my_kus\\links_4th_semester.txt";
-
+	private static String LINKS_PATH = "e:\\data\\my_kus\\links_4th_semester_kus_videos 2211.txt"; // 原始，每门课都各自从 1 开始编号
+	private static String LINKS_PATH_FINAL = "e:\\data\\my_kus\\links_4th_semester_new.txt";
+	private static int[] NODES_NUM = {396, 680, 973, 1073, 1127, 1197}; // {396, 284, 293, 100, 54, 70}
+	private static int[] LINKS_NUM = {405, 726, 1033, 1455, 1775, 2103, 2211}; // {405, 321, 307, 422, 320, 328}
+	
 	public static void main(String[] args) {
-		
 		String result = GenLinks.genLinks();
 		StoreStringIntoFile.storeString(result, LINKS_PATH_FINAL, false);
 	}
 	
 	private static String genLinks() {	
 		String result = "";
-				
+		
 		File file = new File(LINKS_PATH);
 		BufferedReader reader = null;
 		
 		try {
 			reader = new BufferedReader(new FileReader(file));
 			String str = null;
-			int tag = 0;
-		    int tmp_tag = 0;
-			int i = 0;
+			String str1 = null;
+			String str2 = null;
+			int from = 0;
+		    int to = 0;
+			int i = 0; // 文件内容的第 i 行
 			while ((str = reader.readLine()) != null) {
+				str1 = str.split(",")[0];
+				str2 = str.split(",")[1];
 				
-				if (i < 405) { // ku-ku 课程内
-					tag = Integer.parseInt(str.split(",")[0]) - 1;
-					tmp_tag = Integer.parseInt(str.split(",")[1]) - 1;
-				} else if (404 < i && i < 726) {
-					tag = Integer.parseInt(str.split(",")[0]) + 396 - 1;
-					tmp_tag = Integer.parseInt(str.split(",")[1]) + 396 - 1;
-				} else if (725 < i && i < 1033) {
-					tag = Integer.parseInt(str.split(",")[0]) + 680 - 1;
-					tmp_tag = Integer.parseInt(str.split(",")[1]) + 680 - 1;
-				} else if (1032 < i && i < 1455) { // video-ku
-					tag = Integer.parseInt(str.split(",")[0]) + 973 - 1;
-					tmp_tag = Integer.parseInt(str.split(",")[1]) - 1;
-				} else if (1454 < i && i < 1775) {
-					tag = Integer.parseInt(str.split(",")[0]) + 1073 - 1;
-					tmp_tag = Integer.parseInt(str.split(",")[1]) + 396 - 1;
-				} else if (1774 < i && i < 2103) {
-					tag = Integer.parseInt(str.split(",")[0]) + 1127 - 1;
-					tmp_tag = Integer.parseInt(str.split(",")[1]) + 680 - 1;
-				} else { // ku-ku 课程间 108个
-					tag = Integer.parseInt(str.split(",")[0]) - 1;
-					tmp_tag = Integer.parseInt(str.split(",")[1]) - 1;
+				 // ku-ku 课程内 
+				if (i < LINKS_NUM[0]) { // OS
+					from = Integer.parseInt(str1);
+					to = Integer.parseInt(str2);
+				} else if (LINKS_NUM[0] <= i && i < LINKS_NUM[1]) { // CN
+					from = Integer.parseInt(str1) + NODES_NUM[0];
+					to = Integer.parseInt(str2) + NODES_NUM[0];
+				} else if (LINKS_NUM[1] <= i && i < LINKS_NUM[2]) { // JAVA
+					from = Integer.parseInt(str1) + NODES_NUM[1];
+					to = Integer.parseInt(str2) + NODES_NUM[1];
+				}
+				// video-ku
+				else if (LINKS_NUM[2] <= i && i < LINKS_NUM[3]) { // OS
+					from = Integer.parseInt(str1) + NODES_NUM[2];
+					to = Integer.parseInt(str2);
+				} else if (LINKS_NUM[3] <= i && i < LINKS_NUM[4]) { // CN
+					from = Integer.parseInt(str1) + NODES_NUM[3];
+					to = Integer.parseInt(str2) + NODES_NUM[0];
+				} else if (LINKS_NUM[4] <= i && i < LINKS_NUM[5]) { // JAVA
+					from = Integer.parseInt(str1) + NODES_NUM[4];
+					to = Integer.parseInt(str2) + NODES_NUM[1];
+				}
+				// ku-ku 课程间 108个，已经按所有973个知识点统一编号过，无须特殊处理
+				else { 
+					from = Integer.parseInt(str1);
+					to = Integer.parseInt(str2);
 				}
 				
-				result += tag + "," + tmp_tag + "\r\n";
+				result += (from - 1) + "," + (to - 1) + "\r\n"; // 减一保证编号从 0 开始
 	
 				i++;
 			}
-			
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
